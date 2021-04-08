fd57b219935076aa2f8736ec3b5e1f664206393f
Laura <70246252+laura-liber@users.noreply.github.com>
2021-04-07 20:52:06 -0400

    Add files via upload

---
+++ Task Manager/Task Manager/scripts/task.js
@@ -0,0 +1,13 @@
+class Task {
+    constructor(title, description, important, dueDate, location, alertText, status) {
+        this.title = title;
+        this.description = description;
+        this.important = important;
+        this.dueDate = dueDate;
+        this.location = location;
+        this.alertText = alertText;
+        this.status = status;
+
+        this.user = 'LauraL';
+    }
+}
